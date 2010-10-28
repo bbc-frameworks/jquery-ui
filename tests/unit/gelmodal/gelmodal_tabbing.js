4d5ace44d08ebfa34d0f04f946403cff7aa64260
Pete Otaqui <pete.otaqui@bbc.co.uk>
2010-10-28 08:44:55 +0100

    qunit tests for gelmodal tabbing

--- tests/unit/gelmodal/gelmodal_tabbing.js
+++ tests/unit/gelmodal/gelmodal_tabbing.js
@@ -4,19 +4,24 @@
 
 (function($) {
 
+if ( !document.activeElement ) {
+	$(document).bind('focusin', function(event) {
+		document.activeElement = event.originalTarget || event.srcElement;
+	});
+}
+	
+	
 var geloverlay_style_html = '<div class="ui-geloverlay"><a href="#" class="ui-geloverlay-close">X</a><div class="ui-geloverlay-content"><input id="i1" /><input id="i2" tabindex="3" /><input id="i3" /></div></div>';
 
 module('gelmodal: tabbing');
 
 test('focuses the first tabbable element', function() {
-	if ( document.activeElement ) {
-		expect(1);
-		var modal = $('.box-1').gelmodal();
-		modal.gelmodal('open');
-		
-		equals(document.activeElement, $('.box-1 a.close')[0], 'first anchor is focused');
-		modal.gelmodal('destroy');
-	}
+	expect(1);
+	var modal = $('.box-1').gelmodal();
+	modal.gelmodal('open');
+
+	equals(document.activeElement, $('.box-1 a.close')[0], 'first anchor is focused');
+	modal.gelmodal('destroy');
 });
 test('cycles forward after last edge element', function() {
 	
@@ -33,7 +38,7 @@ test('cycling backward works with flash at both ends', function() {
 test('obeys tabIndex without leaving modal', function() {
 	
 });
-test('obeys tabIndex without leaving modal', function() {
+test('obeys backward tabIndex without leaving modal', function() {
 	
 });
 
@@ -42,12 +47,6 @@ test('allows tabbing into focusable flash', function() {
 });
 
 
-test('binds events on open', function() {
-	
-});
-test('unbinds events on close', function() {
-	
-});
 
 
 
